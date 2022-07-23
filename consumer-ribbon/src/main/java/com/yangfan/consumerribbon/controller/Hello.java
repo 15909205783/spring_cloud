a47947c4159c7f8bba0748afc1ffeb867269ad36
15909205783 <2585928225@qq.com>
2022-07-23 21:07:40 +0800

    1、ribbon实现负载均衡

---
+++ consumer-ribbon/src/main/java/com/yangfan/consumerribbon/controller/Hello.java
@@ -0,0 +1,23 @@
+package com.yangfan.consumerribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("foo/")
+public class Hello {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("hello")
+    public String hello() {
+
+        return restTemplate.getForObject("http://PROVIDER-HI/hello", String.class) + " world!";
+    }
+}
